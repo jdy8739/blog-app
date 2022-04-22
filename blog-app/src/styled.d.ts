14c61f09e7a61c76b5bde316f93f18b798ca28b6
= <=>
2022-04-22 17:10:54 +0900

    ..

---
+++ blog-app/src/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    fontColor: string;
+    backgroundColor: string;
+    accentColor: string;
+  };
+};
---
+++ blog-app/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    fontColor: 'black',
+    backgroundColor: 'whitesmoke',
+    accentColor: 'tomato'
+};
+
+export const darkTheme: DefaultTheme = {
+    fontColor: 'whitesmoke',
+    backgroundColor: 'black',
+    accentColor: 'teal'
+};
